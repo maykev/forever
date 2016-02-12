384ad9aadfb5a3b2e2aba3e8c79fa36ceb1cff6f
Kevin May <kevin.may@dbnetworks.com>
2016-02-11 21:34:12 -0800

    allow godaddy iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_action :allow_godaddy_iframe
+
+  private
+
+  def allow_godaddy_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://amyandkevinswedding.com'
+  end
 end
