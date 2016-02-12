bfccf3ba486a76e188b60ff8e4e7c1bbd1afd81b
Kevin May <kevin.may@dbnetworks.com>
2016-02-11 21:41:36 -0800

    allow iframing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  after_action :allow_godaddy_iframe
+  after_action :allow_iframe
 
   private
 
-  def allow_godaddy_iframe
-    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://amyandkevinswedding.com'
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
